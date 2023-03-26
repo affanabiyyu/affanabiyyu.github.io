96277409786d66de6d131974a71520f68c9560f0
affanabiyyu <affanabito.dev@gmail.com>
2023-03-27 04:42:51 +0700

    new templates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,42 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import svelte from "@astrojs/svelte";
-
-// import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import remarkToc from "remark-toc";
+import remarkCollapse from "remark-collapse";
+import sitemap from "@astrojs/sitemap";
+import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://affanabiyyu.github.io',
-  integrations: [mdx(), sitemap(), svelte()]
-  // integrations: [mdx(), sitemap(), svelte(), tailwind()]
+  site: SITE.website,
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    react(),
+    sitemap(),
+  ],
+  markdown: {
+    remarkPlugins: [
+      remarkToc,
+      [
+        remarkCollapse,
+        {
+          test: "Table of contents",
+        },
+      ],
+    ],
+    shikiConfig: {
+      theme: "one-dark-pro",
+      wrap: true,
+    },
+    extendDefaultPlugins: true,
+  },
+  vite: {
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"],
+    },
+  },
 });
